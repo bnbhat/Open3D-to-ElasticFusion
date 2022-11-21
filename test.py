407215584f4353dcf099e8784529f0e9ada81d11
bnbhat <bhalu311@gmail.com>
2022-11-21 02:10:21 +0100

    completed

--- test.py
+++ test.py
@@ -17,6 +17,7 @@ def main():
         timestamp = "0.0"  #timestamp is not available in the log file from Open3D
     
         # Read the file
+        print("Reading file...")
         file = open('trajectory.txt', "r")
         lines = file.readlines()
         file.close()
@@ -25,14 +26,16 @@ def main():
         new_file = open('trajectory'+ ".posegraph", "w")
         
         for mat_line in range(0,len(lines),  5) :
+        #for mat_line in range(0,5,  5) :
             # Get transformation matrix from file
             Tmat = np.zeros((4,4))
 
             iters = 0   # Number of iterations of loop
-            for line in lines[:5]:
-                print(line)
+            for line in lines[:]:
+                #print(line)
                 if iters%5 == 0:  # Every 5th line is the metadata. Skip it
-                    print('skipping line') 
+                    #print('skipping line') 
+                    pass
 
                 else:             # Read the transformation matrix 4 lines after the metadata
                     i_iters = 0
@@ -40,11 +43,11 @@ def main():
                         Tmat[iters%5-1 ,i_iters] = str(x)
 
                         if iters%5-1 == 0 and i_iters == 3:     # Extract the translation tx,ty,tz
-                            tx = str(x)
+                            tx = float(x)
                         elif iters%5-1 == 1 and i_iters == 3:
-                            ty = str(x)
+                            ty = float(x)
                         elif iters%5-1 == 2 and i_iters == 3:
-                            tz = str(x)
+                            tz = float(x)
 
                         i_iters += 1
 
@@ -54,30 +57,18 @@ def main():
 
             # Convert to quaternion
             quterion = sp.spatial.transform.Rotation.from_matrix(Tmat[:3, :3]).as_quat()
-            print(quterion)
-            print(tx,ty,tz)
-            print(type(tx))
-            print(type(quterion[0]))
+            #print(quterion)
+            #print(tx,ty,tz)
+            #print(type(tx))
+            #print(type(quterion[0]))
+            #print(quterion[0])
+            #print(str(quterion[1]))
+            #print(type(str(quterion[1])))
 
             # Write the quaternion and coordinates to the new file
-            new_file.write(timestamp+ " "+ tx + " "+ ty + " " + tz + " " + str(quterion[0]) + " " + str(quterion[1]) + " " + str(quterion[2]) + " " + str(quterion[3]) + "\n") 
+            new_file.write(timestamp+ "\t"+ str(tx) + "\t"+ str(ty) + "\t" + str(tz) + "\t" + str(quterion[0]) + "\t" + str(quterion[1]) + "\t" + str(quterion[2]) + "\t" + str(quterion[3]) + "\n") 
 
 
 main()
-'''
-#read matrix from txt file
-def read_matrix(file):
-    matrix = []
-    for line in file:
-        matrix.append([float(x) for x in line.split()])
-    return matrix
-'''
 
-#read a file from current directory pwd command
-def read_file_pwd(file):
-    pwd = os.getcwd()
-    f = open(pwd + '/' + file, 'r')
-    return f
-def read_file(file):
-    f = open(file, 'r')
-    return f
+print('done')
